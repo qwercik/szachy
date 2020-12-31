c6738b560119512cbc066f3df37fce6e8131c573
qwercik <erykandrzejewski2000@gmail.com>
2020-12-31 17:32:37 +0100

    Add chess pawns icons

---
+++ README.md
@@ -0,0 +1,5 @@
+# szachy
+Simple chess game
+
+## License
+Game is using chess pieces icons from [wikimedia](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces).
--- src/szachy/Main.java
+++ src/szachy/Main.java
@@ -2,18 +2,36 @@ package szachy;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final int WINDOW_WIDTH = 400;
+    private static final int WINDOW_HEIGHT = 400;
+
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setTitle("Szachy");
+
+        StackPane root = new StackPane();
+        Button btn = new Button("Test");
+
+        ButtonType retry = new ButtonType("Test1", ButtonBar.ButtonData.OK_DONE);
+        ButtonType end = new ButtonType("Test2", ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert alert = new Alert(
+                Alert.AlertType.INFORMATION,
+                "Test!",
+                retry, end
+        );
+
+
+        btn.setOnAction(e -> alert.showAndWait());
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setScene(scene);
+
         stage.show();
     }
 
