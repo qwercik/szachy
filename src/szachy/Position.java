80083c1fad3db498a66935e3ac01454c265155c7
qwercik <erykandrzejewski2000@gmail.com>
2020-12-31 17:38:04 +0100

    Add Position class

---
+++ src/szachy/Position.java
@@ -0,0 +1,27 @@
+package szachy;
+
+public class Position {
+    Position(int row, int column) {
+        this.setRow(row);
+        this.setColumn(column);
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    private int row;
+    private int column;
+}
